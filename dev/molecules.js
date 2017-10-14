2faada430855bb53778309e83dfb6f8136a71633
Kaye <suarezka@mail.gvsu.edu>
2017-10-14 08:55:28 -0400

    Initial attempt to reconfigure molecules.

--- dev/molecules.js
+++ dev/molecules.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import React3 from 'react-three-renderer';
+import * as THREE from 'three';
 import {DrawMolecules} from './drawMolecules.js';
 var createReactClass = require('create-react-class');
 
 export var Molecules = createReactClass ({
-    
+
     getInitialState() {
-       return {buffer: this.props.buffer, strong: this.props.strong, mole1: null, mole2: null, mole3: null}
+       return {buffer: this.props.buffer, strong: this.props.strong, mole1: null, mole2: null, mole3: null, cubeRotation: new THREE.Euler()}
     },
 
     componentWillMount() {
+
+        this.cameraPosition = new THREE.Vector3(0, 0, 5); 
+
         this.setState({mole1: this.props.mole1})
         this.setState({mole2: this.props.mole2})
-        this.setState({mole3: this.props.mole3})     
+        this.setState({mole3: this.props.mole3}) 
+
+        this.setState({cubeRotation: new THREE.Euler(this.state.cubeRotation.x + 0.1,
+                        this.state.cubeRotation.y + 0.1, 0)})  
     },
 
     componentDidMount() {
@@ -22,6 +30,7 @@ export var Molecules = createReactClass ({
         var mole1 = split[2];
         var mole2 = split[6];
         var mole3 = this.props.strong;
+        cubeRotation = new THREE.Euler(this.state.cubeRotation.x + 0.1, this.state.cubeRotation.y + 0.1, 0);
 
         console.log(mole1);
         console.log(mole2);
@@ -49,10 +58,50 @@ export var Molecules = createReactClass ({
         var mole2 = split[6];
         var mole3 = this.props.strong;
 
+        const {
+            width,
+            height,
+        } = this.props;
+
+        return (<React3
+              mainCamera="camera" // this points to the perspectiveCamera below
+              width={width}
+              height={height}
+
+              //onAnimate={this._onAnimate}
+            >
+              <scene>
+                <perspectiveCamera
+                  name="camera"
+                  fov={75}
+                  aspect={width / height}
+                  near={0.1}
+                  far={1000}
+
+                  position={this.cameraPosition}
+                />
+                <mesh
+                  rotation={this.state.cubeRotation}
+                >
+                  <boxGeometry
+                    width={1}
+                    height={1}
+                    depth={1}
+                  />
+                  <meshBasicMaterial
+                    color={0x00ff00}
+                  />
+                </mesh>
+              </scene>
+            </React3>);
+        }
+
+/*
         return(
           <div>
             <DrawMolecules mole1={mole1} mole2={mole2} mole3={mole3}/>
           </div>
         )
       }
+      */
     });
