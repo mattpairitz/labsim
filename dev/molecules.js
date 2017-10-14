8977cca629765701648afc24e0a8c7d6ebdc9cbe
Kaye <suarezka@mail.gvsu.edu>
2017-10-14 17:29:43 -0400

    Conflict changes.

--- dev/molecules.js
+++ dev/molecules.js
@@ -8,42 +8,25 @@ var createReactClass = require('create-react-class');
 export var Molecules = createReactClass ({
 
     getInitialState() {
-<<<<<<< HEAD
-       return {buffer: this.props.buffer, strong: this.props.strong, mole1: null, mole2: null, mole3: null, cubeRotation: new THREE.Euler()}
+       return {mole1: null, mole2: null, mole3: null, cubeRotation: new THREE.Euler()}
     },
 
     componentWillMount() {
-
-        this.cameraPosition = new THREE.Vector3(0, 0, 5); 
-
-        this.setState({mole1: this.props.mole1})
-        this.setState({mole2: this.props.mole2})
-        this.setState({mole3: this.props.mole3}) 
-
-        this.setState({cubeRotation: new THREE.Euler(this.state.cubeRotation.x + 0.1,
-                        this.state.cubeRotation.y + 0.1, 0)})  
-    },
-
-    componentDidMount() {
-        /* Split inoming equations into Molecule types */
-=======
-       return {mole1: null, mole2: null, mole3: null}
-    },
-
-    componentWillMount() {
->>>>>>> master
         var buffer = this.props.buffer;
         var [mole1, mole2] = buffer.split(" ");
         var mole3 = this.props.strong;
-<<<<<<< HEAD
-        cubeRotation = new THREE.Euler(this.state.cubeRotation.x + 0.1, this.state.cubeRotation.y + 0.1, 0);
 
-        console.log(mole1);
-        console.log(mole2);
-        console.log(mole3);
-=======
-        this.updateState(mole1, mole2, mole3);
->>>>>>> master
+        this.cameraPosition = new THREE.Vector3(0, 0, 5);
+        this._onAnimate = () => {
+              this.setState({
+                cubeRotation: new THREE.Euler(
+                  this.state.cubeRotation.x + 0.1,
+                  this.state.cubeRotation.y + 0.1,
+                  0
+                ),
+              });
+        };
+       // this.animate();
     },
 
     componentWillReceiveProps(nextProps) {
@@ -52,6 +35,7 @@ export var Molecules = createReactClass ({
         var [mole1, mole2] = buffer.split(" ");
         var mole3 = nextProps.strong;
         this.updateState(mole1, mole2, mole3);
+        //this.animate();
     },
 
     updateState(mole1, mole2, mole3){
@@ -61,25 +45,15 @@ export var Molecules = createReactClass ({
     },
     
     render() {
-<<<<<<< HEAD
-        /* Split inoming equations into Molecule types */
-        var buffer = this.props.buffer;
-        var split = buffer.split(" ");
-        var mole1 = split[2];
-        var mole2 = split[6];
-        var mole3 = this.props.strong;
-
-        const {
-            width,
-            height,
-        } = this.props;
+        var width = 300;
+        var height = 300;
 
         return (<React3
               mainCamera="camera" // this points to the perspectiveCamera below
               width={width}
               height={height}
 
-              //onAnimate={this._onAnimate}
+              onAnimate={this._onAnimate()}
             >
               <scene>
                 <perspectiveCamera
@@ -105,16 +79,14 @@ export var Molecules = createReactClass ({
                 </mesh>
               </scene>
             </React3>);
-        }
 
-/*
-=======
->>>>>>> master
+        /*
         return(
           <div>
             <DrawMolecules mole1={this.state.mole1} mole2={this.state.mole2} mole3={this.state.mole3}/>
           </div>
         )
-      }
-      */
-    });
+*/
+    }
+
+});
