899a188d8024231133e90db0251805f6b8c0c616
Kaye <suarezka@mail.gvsu.edu>
2017-11-06 17:04:54 -0500

    Fixed positions of molecules above equations.

--- dev/drawMolecules.js
+++ dev/drawMolecules.js
@@ -36,9 +36,9 @@ getInitialState() {
  drawGroups(buff1, strong, ctx) {
 
     const pos1 = {x: 50, y: 50};
-    const pos2 = {x: 150, y: 40};
-    const pos3 = {x: 250, y: 25};
-    const pos4 = {x: 320, y: 40};
+    const pos2 = {x: 150, y: 50};
+    const pos3 = {x: 250, y: 50};
+    const pos4 = {x: 350, y: 50};
     const smRadius = 10;
     const lgRadius = 20;
     const total = smRadius + lgRadius;
@@ -72,61 +72,15 @@ getInitialState() {
         break;
 
         case "NH\u2084Cl":
-            const xPos = [-30, 0, 30, 0];
-            const yPos = [0, 30, 0, -30];
-
-            for (var i = 0; i < 4; i++) {
-                this.drawH(pos1.x + xPos[i], pos1.y + yPos[i], smRadius, ctx);
-            }
-
-            for (var i = 0; i < 3; i++) {
-                this.drawH(pos3.x + xPos[i], pos3.y + yPos[i], smRadius, ctx);
-            }
-
-            this.drawN(pos1.x, pos1.y, lgRadius, ctx);
-            this.drawCl(pos2.x, pos2.y, lgRadius, ctx);
-            this.drawN(pos3.x, pos3.y, lgRadius, ctx);
-
+            this.drawNH3(pos1.x, pos1.y, ctx);
+            this.drawH2O(pos2.x, pos2.y, ctx);
+            this.drawO(pos3.x, pos3.y, lgRadius, ctx);
+            this.drawH(pos3.x + total, pos3.y, smRadius, ctx);
+            this.drawNH4(pos4.x, pos4.y, ctx);
 
         break;
     }
 
-/*
-    switch (buff2) {
-        case "NaA":
-            this.drawNa(pos2.x, pos2.y, lgRadius, ctx);
-            this.drawA(pos2.x, pos2.y, lgRadius, ctx);
-
-        break;
-
-        case "NaF":
-            this.drawNa(120, 210, 20, ctx);
-            this.drawF(160, 210, 20, ctx);
-
-        break;
-
-        case "NaClO":
-            this.drawNa(120, 210, 20, ctx);
-            this.drawO(210, 210, 20, ctx);
-            this.drawCl(180, 210, 20, ctx);
-
-        break;
-
-        case "NH\u2083":
-            const numH = 3;
-
-            const xPos = [120, 150, 180];
-            const yPos = [180, 210, 180];
-
-            for (var i = 0; i < numH; i++) {
-                this.drawH(xPos[i], yPos[i], 10, ctx);
-            }
-
-            this.drawN(150, 180, 20, ctx);
-
-        break;
-    }
-*/
     switch (strong) {
         case "None":
             //this.drawH20(250, 50, ctx);
@@ -146,20 +100,39 @@ getInitialState() {
  },
 
 
- /***************************** H20 MOLECULE ********/
+ /***************************** H2O MOLECULE ********/
 drawH2O(xPos, yPos, ctx) {
     this.drawO(xPos, yPos, 20, ctx);
     this.drawH(xPos - 25, yPos + 15, 10, ctx);
     this.drawH(xPos + 25, yPos + 15, 10, ctx);
 },
 
-/****************************** H30 MOLECULE ********/
+/****************************** H3O MOLECULE ********/
 drawH3O(xPos, yPos, ctx) {
     this.drawO(xPos, yPos, 20, ctx);
     this.drawPosCharge(xPos, yPos, ctx);
     this.drawH(xPos - 30, yPos, 10, ctx);
     this.drawH(xPos, yPos + 30, 10, ctx);
     this.drawH(xPos + 30, yPos, 10, ctx);
+},
+
+/****************************** NH3 MOLECULE ********/
+drawNH3(xPos, yPos, ctx) {
+    this.drawN(xPos, yPos, 20, ctx);
+    this.drawPosCharge(xPos, yPos, ctx);
+    this.drawH(xPos - 30, yPos, 10, ctx);
+    this.drawH(xPos, yPos + 30, 10, ctx);
+    this.drawH(xPos + 30, yPos, 10, ctx);
+},
+
+/****************************** NH4 MOLECULE ********/
+drawNH4(xPos, yPos, ctx) {
+    this.drawN(xPos, yPos, 20, ctx);
+    this.drawPosCharge(xPos, yPos, ctx);
+    this.drawH(xPos - 30, yPos, 10, ctx);
+    this.drawH(xPos, yPos + 30, 10, ctx);
+    this.drawH(xPos + 30, yPos, 10, ctx);
+    this.drawH(xPos, yPos - 30, 10, ctx);
 },
 
 /****************************** HCL MOLECULE *********/
@@ -183,7 +156,8 @@ drawNaOH(xPos, yPos, ctx) {
     /* H Molecule */
     ctx.beginPath();
     ctx.arc(xPos, yPos, size, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fill();
 
     /* Draw '+' sign */
     this.drawPosCharge(xPos, yPos, ctx);
@@ -285,7 +259,7 @@ drawNegCharge(xPos, yPos, ctx) {
 render() {
     return(
           <div>
-            <canvas ref="canvas" width={350} height={100} />
+            <canvas ref="canvas" width={400} height={100} />
           </div>
         )
 }
